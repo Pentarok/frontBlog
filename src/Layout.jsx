879246c1bf6f361cea1dd84df56aeb39be55e734
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-09-30 12:18:45 +0300

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,42 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import ResponsiveAppBar from './NavigationBar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 const Layout = () => {
 
-  
+    const navigate=useNavigate();
+  const serverUri = import.meta.env.VITE_SERVER;
+  const [error,setError]=useState(false)
+ const validateUser=async ()=>{
+  try {
+    const res= await axios.get(`${serverUri}/verifyuser`,{  withCredentials: true });
+    console.log(res)
+    if(res.data !=="Success"){
+       setError(true) 
+    }
+   
+   } catch (error) {
+     console.log(error)
+     setError(true)
+   }
+ }
+ useEffect(()=>{
+validateUser();
+ },[]) 
+ if(error){
+  return <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
+  <div className='text-white mb-3'>An error occurred!</div>
+  <div>
+    <Link className='btn btn-primary text-white' to='/login'>Please try logging in again</Link>
+  </div>
+</div>
+
+
+ }
   return (
     <div>
        
